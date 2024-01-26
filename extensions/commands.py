73243a033e30e4e002198407c4918ad56515dd0b
unknown <reallywhoknows>
2024-01-25 22:42:14 -0600

    Fixed an issue where searchs were breaking in the for loop instead of after completion, also added a command to look up highscores of players

--- extensions/commands.py
+++ extensions/commands.py
@@ -1,20 +1,54 @@
 import requests
+import json
 import discord
 from discord.commands import slash_command
 from discord.ext import commands
 from datetime import date
 
+headers = {
+        "User-Agent": "RuneInfo",
+        "From": "https://github.com/reallywhoknows/RuneInfo"
+    }
+
 class commands(commands.Cog):
     def __init__(self,client):
         self.client = client
 
+    @slash_command(guild_ids=[1087515364017066135],name='lookup',description='Check the stats of any OSRS user!')
+    async def lookup(self,ctx: discord.ApplicationContext,rsn):
+
+        with requests.Session() as session:
+            request = session.get(f"https://secure.runescape.com/m=hiscore_oldschool/index_lite.json?player={rsn.replace(' ', '%20')}",headers=headers)
+        json_data = request.json()
+
+        stats = {}
+
+        for i in json_data['skills']:
+            stats[i['name']] = i['level']
+        
+        print(stats)
+        #Unfortunately due to limitations, this is going to get a bit ugly. We will need to reference a custom emoji to include the icon in an embed.
+        #File uploads are restricted 10 per message & URLs don't embed the icon and instead show as hyperlink.
+        #If there is a better way to do this please let me know.
+
+        with open("./assets/resource.json") as content:
+            icons = json.load(content)
+
+        embed = discord.Embed(
+            title = f"**{rsn} {icons['overall']} {stats['Overall']}**",
+            description = f"",
+            color = discord.Colour.brand_red(),
+            )
+        embed.add_field(name="", value=f"{icons['attack']} {stats['Attack']}\n{icons['strength']} {stats['Strength']}\n{icons['defence']} {stats['Defence']}\n{icons['ranged']} {stats['Ranged']}\n{icons['prayer']} {stats['Prayer']}\n{icons['runecraft']} {stats['Runecraft']}\n{icons['construction']} {stats['Construction']}", inline=True)
+        embed.add_field(name="", value=f"{icons['hitpoints']} {stats['Hitpoints']}\n{icons['agility']} {stats['Agility']}\n{icons['herblore']} {stats['Herblore']}\n{icons['thieving']} {stats['Thieving']}\n{icons['crafting']} {stats['Crafting']}\n{icons['fletching']} {stats['Fletching']}\n{icons['slayer']} {stats['Slayer']}\n{icons['hunter']} {stats['Hunter']}", inline=True)
+        embed.add_field(name="", value=f"{icons['mining']} {stats['Mining']}\n{icons['smithing']} {stats['Smithing']}\n{icons['fishing']} {stats['Fishing']}\n{icons['cooking']} {stats['Cooking']}\n{icons['firemaking']} {stats['Firemaking']}\n{icons['woodcutting']} {stats['Woodcutting']}\n{icons['farming']} {stats['Farming']}", inline=True)
+        embed.set_footer(text=date.today().strftime("%B %d, %Y"))
+
+        await ctx.respond(embed=embed)
+
     @slash_command(guild_ids=[1087515364017066135],name='price',description='Get the latest buy/sell information for the item of your choice.')
     async def price(self,ctx: discord.ApplicationContext,item):
         #Add headers, or osrs api will complain. Ref github repo for contact information.
-        headers = {
-            "User-Agent": "RuneInfo",
-            "From": "https://github.com/reallywhoknows/RuneInfo"
-        }
 
         #GET api request, convert into JSON data for usage.
         with requests.Session() as session:
@@ -36,19 +70,20 @@ class commands(commands.Cog):
                 item_id = i["id"]
                 item_name = i["name"]
                 item_examine = i["examine"]
-            else:
-                #Build the error message with styling for more pleasant presentation
-                embed = discord.Embed(
-                    title = "Error! Item not found!",
-                    description = f"Please check if `{item}` is a valid item name or id otherwise please report the issue.\nhttps://github.com/reallywhoknows/RuneInfo/issues",
-                    color = discord.Colour.brand_red(),
-                    )
-                file = discord.File("./assets/warning.png")
-                embed.set_thumbnail(url="attachment://warning.png")
-                embed.set_footer(text=date.today().strftime("%B %d, %Y"))
+                break
+        else:
+            #Build the error message with styling for more pleasant presentation
+            embed = discord.Embed(
+                title = "Error! Item not found!",
+                description = f"Please check if `{item}` is a valid item name or id otherwise please report the issue.\nhttps://github.com/reallywhoknows/RuneInfo/issues",
+                color = discord.Colour.brand_red(),
+                )
+            file = discord.File("./assets/warning.png")
+            embed.set_thumbnail(url="attachment://warning.png")
+            embed.set_footer(text=date.today().strftime("%B %d, %Y"))
 
-                await ctx.respond(embed=embed, file=file)
-                return
+            await ctx.respond(embed=embed, file=file)
+            return
 
         #Some URLs do not like passing integers in the url.
         item_id = str(item_id)
